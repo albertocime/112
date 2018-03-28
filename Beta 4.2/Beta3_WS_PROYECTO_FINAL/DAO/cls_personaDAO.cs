4fffd8d8713c9faeabc096fe2bebf5a94f57434c
gaspar <gaspar@DELTA1>
2018-03-28 15:12:03 -0600

    Se agrego: ModificarDT en cls_personaDAO

--- Beta 4.2/Beta3_WS_PROYECTO_FINAL/DAO/cls_personaDAO.cs
+++ Beta 4.2/Beta3_WS_PROYECTO_FINAL/DAO/cls_personaDAO.cs
@@ -27,9 +27,13 @@ namespace Beta3_WS_PROYECTO_FINAL.DAO
 
         public int AgregarDt(cls_personaBO obj_persona)
         {
-            String ComandoSQL = string.Format("INSERT INTO  persona (matricula,Nombre,Apelido_Paterno,Apelido_Materno,Correo_Electronico,Telefono,Genero,tipo_persona,id_usuario,Idequipo,Idconvocatoria)VALUES ({0},'{1}','{2}',{3},'{4}','{5}',{6},{7});", obj_alumbo.Matricula, obj_alumbo.Nombre, obj_alumbo.Apellido, obj_alumbo.Id_div, obj_alumbo.Fechanac.ToString("yyyy-MM-dd HH:mm:ss"));
+            String ComandoSQL = string.Format("INSERT INTO  persona (matricula,Nombre,Apelido_Paterno,Apelido_Materno,Correo_Electronico,Telefono,Genero,tipo_persona,id_usuario,Idequipo,Idconvocatoria)VALUES ({0},'{1}','{2}',{3},'{4}','{5}',{6},{7},{8},{9},{10});", obj_per.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona,obj_persona.Genero_persona,obj_persona.Tipo_persona,obj_persona.Id_usuario,obj_persona.Id_equipo,obj_persona.Id_convocatoria);
             return Miconexion.EjecutarComando(ComandoSQL);
         }
-
+        public int ModificarDt(cls_personaBO obj_persona)
+        {
+            string ComandoSQL = string.Format("UPDATE persona SET Nombre='{1}',Apelido_Paterno='{2}',Apelido_Materno='{3}',Correo_Electronico='{4}',Telefono='{5}',Genero={6},tipo_persona={7},id_usuario={8},Idequipo={9},Idconvocatoria={10}", obj_persona.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona, obj_persona.Genero_persona, obj_persona.Tipo_persona, obj_persona.Id_usuario, obj_persona.Id_equipo, obj_persona.Id_convocatoria);
+            return Miconexion.EjecutarComando(ComandoSQL);
+        }
     }
 }
