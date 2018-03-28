152fbc31b1c221d6c29b5d7f68ee3cd2f4a714bc
Albertocime <albertocimecastellanos@gmail.com>
2018-03-28 16:47:57 -0600

    Bryan prueba

---
+++ Beta 4.2/Beta3_WS_PROYECTO_FINAL/BO/Clsbryan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beta3_WS_PROYECTO_FINAL.BO
+{
+    class Clsbryan
+    {
+    }
+}
